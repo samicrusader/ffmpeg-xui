99aa97fc250d7ef16155673cfc0ed8893f23d656
samicrusader <hi@samicrusader.me>
2025-01-15 00:23:01 -0500

    disable avc/hevc bitstream check

--- libavformat/mpegtsenc.c
+++ libavformat/mpegtsenc.c
@@ -1587,6 +1587,7 @@ static void mpegts_write_pes(AVFormatContext *s, AVStream *st,
 
 int ff_check_h264_startcode(AVFormatContext *s, const AVStream *st, const AVPacket *pkt)
 {
+#if 0
     if (pkt->size < 5 || AV_RB32(pkt->data) != 0x0000001 && AV_RB24(pkt->data) != 0x000001) {
         if (!st->nb_frames) {
             av_log(s, AV_LOG_ERROR, "H.264 bitstream malformed, "
@@ -1599,11 +1600,13 @@ int ff_check_h264_startcode(AVFormatContext *s, const AVStream *st, const AVPack
             av_log(s, AV_LOG_WARNING, " data %08"PRIX32, AV_RB32(pkt->data));
         av_log(s, AV_LOG_WARNING, "\n");
     }
+#endif
     return 0;
 }
 
 static int check_hevc_startcode(AVFormatContext *s, const AVStream *st, const AVPacket *pkt)
 {
+#if 0
     if (pkt->size < 5 || AV_RB32(pkt->data) != 0x0000001 && AV_RB24(pkt->data) != 0x000001) {
         if (!st->nb_frames) {
             av_log(s, AV_LOG_ERROR, "HEVC bitstream malformed, no startcode found\n");
@@ -1614,6 +1617,7 @@ static int check_hevc_startcode(AVFormatContext *s, const AVStream *st, const AV
             av_log(s, AV_LOG_WARNING, " data %08"PRIX32, AV_RB32(pkt->data));
         av_log(s, AV_LOG_WARNING, "\n");
     }
+#endif
     return 0;
 }
 
